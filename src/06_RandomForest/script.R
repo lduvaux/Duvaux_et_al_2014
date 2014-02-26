a7cb7ed7ba11ffba495ae2bec2880227f6b75daf
lduvaux <l.duvaux@sheffield.ac.uk>
2014-02-26 13:34:43 +0000

    miscellaneous cleanings

--- src/06_RandomForest/script.R
+++ src/06_RandomForest/script.R
@@ -12,166 +12,152 @@ source("../utils/randomForest_helperFuns.R")
 source("./params.R")
 source("./functions.R")
 
-#~main <- function(argv){
+set.seed(0)
 
-	load(PREVIOUS_DATA)
+load(PREVIOUS_DATA)
 
-    races <- PrePro_fetchRaces(RAW_DATA, CTRL_GUYS , BAD_GUYS)
-    races[races=="Medicago_ctrl"] <- "Medicago"
-	races_uniq <- PrePro_fetchUniqRaces(races)
-
-	set.seed(1)
-	pure_indiv <- names(y)
-	ind_pure_indiv <- PrePro_findIndex(pure_indiv, indiv)
+races <- PrePro_fetchRaces(RAW_DATA, CTRL_GUYS , BAD_GUYS)
+races[races=="Medicago_ctrl"] <- "Medicago"
+races_uniq <- PrePro_fetchUniqRaces(races)
 
 
-        cat("\n")
-	print("###### 1) run a RF by removing all noisy loci ######")
-			# 1.1) extract locus importance from the biggest RF
-    temp_tab <- sapply(gradNtree_lrf_unsuperv, function(x) x$importance[,"MeanDecreaseGini"])
-    gini_NmaxTrees <- temp_tab[, ncol(temp_tab)]
-    mat_imp_NmaxTrees <- gradNtree_lrf_unsuperv[[length(gradNtree_lrf_unsuperv)]]$importance
-
-        # 1.2) remove uninformative variables (i.e. baits)
-	bad <- which(gini_NmaxTrees==0)
-
-    gini_NmaxTrees1 <- gini_NmaxTrees[-bad]
-    new_x <- x[,-bad]
-
-        # 1.3) keep only the better bait of each informative exon
-    data_PerExons <- Keep_MaxBait(gini_NmaxTrees1, new_x)
-
-        # 1.4) run the RF
-	new_x1 <- data_PerExons$new_xf
-	new_xtest1 <- xtest[,PrePro_findIndex(colnames(new_x1), colnames(xtest))]
-	
-	print(sprintf("Growing random forest of %i trees...", NTREES))
-    exon_rf <- randomForest(x=new_x1, y=y, xtest=new_xtest1, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
-    print(exon_rf)
-
-        # 1.5) extract best baits per exon
-    gini_exon_rf0 <- exon_rf$importance[,"MeanDecreaseGini"]
-    N_cate_rfEx <- table(sapply(names(gini_exon_rf0), get_elements))
-
-    gini_exon_rf <- sort(gini_exon_rf0, decreasing=T)
-    gini_exon_rf_ind <- order(gini_exon_rf0, decreasing=T)
-    best50_ExonPromot <- names(gini_exon_rf[1:50])
-    tab_best50 <- (exon_rf$importance[gini_exon_rf_ind,])[1:50,]
-    geneOfbest50 <- unique(sapply(best50_ExonPromot, get_elements, sep="\\."))
-	
-	tem_tab <- cbind(rownames(tab_best50), round(tab_best50,4))
-	write.table(tem_tab, TAB50EXON, quote=T, sep="\t", row.names=F)
-	tem_tab <- exon_rf$importance[gini_exon_rf_ind,]
-	tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
-	write.table(tem_tab, TABIMPEXON, quote=T, sep="\t", row.names=F)
+pure_indiv <- names(y)
+ind_pure_indiv <- PrePro_findIndex(pure_indiv, indiv)
 
 
-        cat("\n")
-	print("###### 2) run a RF with one bait per gene ######")
-			# 2.1) rm non interesting baits
-	data_PerGene <- Keep_MaxBait_PerGene(gini_exon_rf0, new_x1)
-    
-        # 2.2) run the RF
-    new_x2 <- data_PerGene$mat_x
-	new_xtest2 <- xtest[,PrePro_findIndex(colnames(new_x2), colnames(xtest))]
-	
-    print(sprintf("Growing random forest of %i trees...", NTREES))
-    gene_rf <- randomForest(x=new_x2, y=y, xtest=new_xtest2, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
-    print(gene_rf)
-    
-        # 2.3) extract best baits per gene
-    gini_gene_rf <- sort(gene_rf$importance[,10], decreasing=T)
-    N_cate_rfGn <- table(sapply(names(gini_gene_rf), get_elements)) # give the total number of gene baits and promoter baits to be used in the bootstrap analysis
+    cat("\n")
+print("###### 1) run a RF by removing all noisy loci ######")
+        # 1.1) extract locus importance from the biggest RF
+temp_tab <- sapply(gradNtree_lrf_unsuperv, function(x) x$importance[,"MeanDecreaseGini"])
+gini_NmaxTrees <- temp_tab[, ncol(temp_tab)]
+mat_imp_NmaxTrees <- gradNtree_lrf_unsuperv[[length(gradNtree_lrf_unsuperv)]]$importance
 
-    gini_gene_rf_ind <- order(gene_rf$importance[,10], decreasing=T)
-    best20_GenePromot <- names(gini_gene_rf[1:20])
-    tab_best20_genes <- (gene_rf$importance[gini_gene_rf_ind,])[1:20,]
-	
-	tem_tab <- cbind(rownames(tab_best20_genes), round(tab_best20_genes,4))
-	write.table(tem_tab, TAB20EXON, quote=T, sep="\t", row.names=F)
-	tem_tab <- gene_rf$importance[gini_gene_rf_ind,]
-	tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
-	write.table(tem_tab, TABIMPGn, quote=T, sep="\t", row.names=F)
+    # 1.2) remove uninformative variables (i.e. baits)
+bad <- which(gini_NmaxTrees==0)
 
+gini_NmaxTrees1 <- gini_NmaxTrees[-bad]
+new_x <- x[,-bad]
 
-        cat("\n")
-	print("###### 3) run a RF with one bait per contig (either PMT either exon) ######")
-        # 3.1) rm non interesting baits
-	all_targ <- sapply(names(gini_gene_rf), Pro_ExonName)
-	infoTargGene <- get_info_AllTargGene(INFO_TARGENE_FILE, all_targ)
-	bestBait_PerContig <- as.character(get_1baitPerContig(infoTargGene, gini_gene_rf))
-	
-        # 3.2) run the RF
-	ind <- PrePro_findIndex(bestBait_PerContig, colnames(new_x2))
-	new_x3 <- new_x2[,ind]
-	new_xtest3 <- xtest[,PrePro_findIndex(colnames(new_x3), colnames(xtest))]
-    print(sprintf("Growing random forest of %i trees...", NTREES))
-    contig_rf <- randomForest(x=new_x3, y=y, xtest=new_xtest3, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
-        # export RF contig results
-    tab <- cbind(rownames(contig_rf$votes), contig_rf$votes)
-    write.table(tab, file=TRAIN_SET_VOTES, sep="\t", quote=F, row.names=F)
-    tab <- cbind(rownames(contig_rf$test$votes), contig_rf$test$votes)
-    write.table(tab, file=TEST_SET_VOTES, sep="\t", quote=F, row.names=F)
+    # 1.3) keep only the better bait of each informative exon
+data_PerExons <- Keep_MaxBait(gini_NmaxTrees1, new_x)
 
-    print(contig_rf)
+    # 1.4) run the RF
+new_x1 <- data_PerExons$new_xf
+new_xtest1 <- xtest[,PrePro_findIndex(colnames(new_x1), colnames(xtest))]
 
-        # 3.3) extract best baits per contig
-    gini_contig_rf <- sort(contig_rf$importance[,10], decreasing=T)
-    gini_contig_rf_ind <- order(contig_rf$importance[,10], decreasing=T)
-    best20_contigPromot <- names(gini_contig_rf[1:20])
-    tab_best20_contig <- (contig_rf$importance[gini_contig_rf_ind,])[1:20,]
-	
-	tem_tab <- cbind(rownames(tab_best20_contig), round(tab_best20_contig,4))
-	write.table(tem_tab, TAB20CONTIG, quote=F, sep="\t", row.names=F)
-	tem_tab <- contig_rf$importance[gini_contig_rf_ind,]
-	tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
-	write.table(tem_tab, TABIMPCONTIG, quote=F, sep="\t", row.names=F)
-    
-    indiv_votes <- round(contig_rf$votes,2)
-    test_indiv_votes <- round(contig_rf$test$votes,2)
-    tvotes <- rbind(cbind(rownames(indiv_votes),indiv_votes), cbind(rownames(test_indiv_votes), test_indiv_votes))
-    indiv_predic <- as.character(contig_rf$predicted)
-    test_indiv_predic <- as.character(contig_rf$test$predicted)
-    tab <-  cbind(tvotes, c(indiv_predic, test_indiv_predic))
-    write.table(tab, ASSIGN_TAB, quote=F, sep="\t", row.names=F)
+print(sprintf("Growing random forest of %i trees...", NTREES))
+exon_rf <- randomForest(x=new_x1, y=y, xtest=new_xtest1, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
+print(exon_rf)
+
+    # 1.5) extract best baits per exon
+gini_exon_rf0 <- exon_rf$importance[,"MeanDecreaseGini"]
+N_cate_rfEx <- table(sapply(names(gini_exon_rf0), get_elements))
+
+gini_exon_rf <- sort(gini_exon_rf0, decreasing=T)
+gini_exon_rf_ind <- order(gini_exon_rf0, decreasing=T)
+best50_ExonPromot <- names(gini_exon_rf[1:50])
+tab_best50 <- (exon_rf$importance[gini_exon_rf_ind,])[1:50,]
+geneOfbest50 <- unique(sapply(best50_ExonPromot, get_elements, sep="\\."))
+
+tem_tab <- cbind(rownames(tab_best50), round(tab_best50,4))
+write.table(tem_tab, TAB50EXON, quote=T, sep="\t", row.names=F)
+tem_tab <- exon_rf$importance[gini_exon_rf_ind,]
+tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
+write.table(tem_tab, TABIMPEXON, quote=T, sep="\t", row.names=F)
 
 
-    print("###### 4) check CN for best baits and their contigous baits ######")
-		# 4.1) chose relevant individuals
-    good_indiv <- which(as.character(y)==contig_rf$predicted)
-    x_prim <- x[good_indiv,]
-    y_prim <- y[good_indiv]
-    
-		# 4.2) index for races
-	ind_races <- mclapply(races_uniq, grep, rownames(x_prim))
-	names(ind_races) <- races_uniq
-		# 4.3) check the best 20 baits
-	bests20 <- rownames(tab_best20_contig)
+    cat("\n")
+print("###### 2) run a RF with one bait per gene ######")
+        # 2.1) rm non interesting baits
+data_PerGene <- Keep_MaxBait_PerGene(gini_exon_rf0, new_x1)
 
-    system("mkdir -p Res_RF_bestGenes/")
-    sapply(1:length(bests20), function(x) check_baits4CN(bests20[x], x_prim=x_prim, y_prim=y_prim, contig_rf=contig_rf, mat_imp_NmaxTrees=mat_imp_NmaxTrees, info_TarGene_file=INFO_TARGENE_FILE, ind_races=ind_races, outdir="./Res_RF_bestGenes/", rang=x,races_uniq = races_uniq))
+    # 2.2) run the RF
+new_x2 <- data_PerGene$mat_x
+new_xtest2 <- xtest[,PrePro_findIndex(colnames(new_x2), colnames(xtest))]
 
-		# 4.4) the 4 best loci per race
-	contig_rf_imp <- contig_rf$importance[,-(9:10)]
-	check_baits4CN_perRace(contig_rf_imp, x_prim, y_prim=y_prim, contig_rf, mat_imp_NmaxTrees, INFO_TARGENE_FILE, ind_races, outdir="./Res_RF_bestGenes/", n_best=4,races_uniq)
-	
-    argv <- commandArgs(TRUE)[1]
-	outFileName <- argv[1]
-    ver(sprintf("Saving data to %s",outFileName))
-    rm(argv)
-    save.image(file=outFileName)
-#~}
+print(sprintf("Growing random forest of %i trees...", NTREES))
+gene_rf <- randomForest(x=new_x2, y=y, xtest=new_xtest2, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
+print(gene_rf)
 
-#~if(DEBUG)
-#~	traceback(main(argv));
-#~if(!DEBUG)
-#~	main(argv);
+    # 2.3) extract best baits per gene
+gini_gene_rf <- sort(gene_rf$importance[,10], decreasing=T)
+N_cate_rfGn <- table(sapply(names(gini_gene_rf), get_elements)) # give the total number of gene baits and promoter baits to be used in the bootstrap analysis
+
+gini_gene_rf_ind <- order(gene_rf$importance[,10], decreasing=T)
+best20_GenePromot <- names(gini_gene_rf[1:20])
+tab_best20_genes <- (gene_rf$importance[gini_gene_rf_ind,])[1:20,]
+
+tem_tab <- cbind(rownames(tab_best20_genes), round(tab_best20_genes,4))
+write.table(tem_tab, TAB20EXON, quote=T, sep="\t", row.names=F)
+tem_tab <- gene_rf$importance[gini_gene_rf_ind,]
+tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
+write.table(tem_tab, TABIMPGn, quote=T, sep="\t", row.names=F)
 
 
+    cat("\n")
+print("###### 3) run a RF with one bait per contig (either PMT either exon) ######")
+    # 3.1) rm non interesting baits
+all_targ <- sapply(names(gini_gene_rf), Pro_ExonName)
+infoTargGene <- get_info_AllTargGene(INFO_TARGENE_FILE, all_targ)
+bestBait_PerContig <- as.character(get_1baitPerContig(infoTargGene, gini_gene_rf))
+
+    # 3.2) run the RF
+ind <- PrePro_findIndex(bestBait_PerContig, colnames(new_x2))
+new_x3 <- new_x2[,ind]
+new_xtest3 <- xtest[,PrePro_findIndex(colnames(new_x3), colnames(xtest))]
+print(sprintf("Growing random forest of %i trees...", NTREES))
+contig_rf <- randomForest(x=new_x3, y=y, xtest=new_xtest3, ytest=ytest, ntree=NTREES, proximity = TRUE, importance=T, sampsize=SAMP_SIZE2)
+    # export RF contig results
+tab <- cbind(rownames(contig_rf$votes), contig_rf$votes)
+write.table(tab, file=TRAIN_SET_VOTES, sep="\t", quote=F, row.names=F)
+tab <- cbind(rownames(contig_rf$test$votes), contig_rf$test$votes)
+write.table(tab, file=TEST_SET_VOTES, sep="\t", quote=F, row.names=F)
+
+print(contig_rf)
+
+    # 3.3) extract best baits per contig
+gini_contig_rf <- sort(contig_rf$importance[,10], decreasing=T)
+gini_contig_rf_ind <- order(contig_rf$importance[,10], decreasing=T)
+best20_contigPromot <- names(gini_contig_rf[1:20])
+tab_best20_contig <- (contig_rf$importance[gini_contig_rf_ind,])[1:20,]
+
+tem_tab <- cbind(rownames(tab_best20_contig), round(tab_best20_contig,4))
+write.table(tem_tab, TAB20CONTIG, quote=F, sep="\t", row.names=F)
+tem_tab <- contig_rf$importance[gini_contig_rf_ind,]
+tem_tab <- cbind(rownames(tem_tab), round(tem_tab,4))
+write.table(tem_tab, TABIMPCONTIG, quote=F, sep="\t", row.names=F)
+
+indiv_votes <- round(contig_rf$votes,2)
+test_indiv_votes <- round(contig_rf$test$votes,2)
+tvotes <- rbind(cbind(rownames(indiv_votes),indiv_votes), cbind(rownames(test_indiv_votes), test_indiv_votes))
+indiv_predic <- as.character(contig_rf$predicted)
+test_indiv_predic <- as.character(contig_rf$test$predicted)
+tab <-  cbind(tvotes, c(indiv_predic, test_indiv_predic))
+write.table(tab, ASSIGN_TAB, quote=F, sep="\t", row.names=F)
 
 
+print("###### 4) check CN for best baits and their contigous baits ######")
+    # 4.1) chose relevant individuals
+good_indiv <- which(as.character(y)==contig_rf$predicted)
+x_prim <- x[good_indiv,]
+y_prim <- y[good_indiv]
 
+    # 4.2) index for races
+ind_races <- mclapply(races_uniq, grep, rownames(x_prim))
+names(ind_races) <- races_uniq
+    # 4.3) check the best 20 baits
+bests20 <- rownames(tab_best20_contig)
 
+system("mkdir -p Res_RF_bestGenes/")
+sapply(1:length(bests20), function(x) check_baits4CN(bests20[x], x_prim=x_prim, y_prim=y_prim, contig_rf=contig_rf, mat_imp_NmaxTrees=mat_imp_NmaxTrees, info_TarGene_file=INFO_TARGENE_FILE, ind_races=ind_races, outdir="./Res_RF_bestGenes/", rang=x,races_uniq = races_uniq))
 
+    # 4.4) the 4 best loci per race
+contig_rf_imp <- contig_rf$importance[,-(9:10)]
+check_baits4CN_perRace(contig_rf_imp, x_prim, y_prim=y_prim, contig_rf, mat_imp_NmaxTrees, INFO_TARGENE_FILE, ind_races, outdir="./Res_RF_bestGenes/", n_best=4,races_uniq)
 
+argv <- commandArgs(TRUE)[1]
+outFileName <- argv[1]
+ver(sprintf("Saving data to %s",outFileName))
+rm(argv)
+save.image(file=outFileName)
 
--- src/08_GeneFamImptceTest/script.R
+++ src/08_GeneFamImptceTest/script.R
@@ -15,127 +15,120 @@ load(PREVIOUS_DATA)
 source("params.R")  # needed as the previous will load former params!
 source("./functions.R")
 
-#~main <- function(argv){
+set.seed(0)
 
-    cat("\n")
-    print("########################################################
-    Perform test to detect gene category with significant effect to distinguish races
-    ########################################################")
-
-
-    cat("\n")
-    print("##### 1) observed sum of ranks")
-	genes <- addZeroImpGenes(gini_contig_rf)
-    group <- sapply(names(genes), get_elements)
-    categ <- sort(unique(group))
-    final_nber <- length(gini_contig_rf)
-    sum_ranks <- get_rk_sum(names(genes), n_imp=final_nber, kept=length(genes), categ)
-    sum_ranks <- data.frame(grp=categ, rnk=sum_ranks)
-    rownames(sum_ranks) <- categ
+cat("\n")
+print("########################################################
+Perform test to detect gene category with significant effect to distinguish races
+########################################################")
 
 
-    cat("\n")
-    print("##### 2) P being same contig")
-    nn <- paste(sapply(names(genes), collapse_elements, sep="_", what=1:2, colla="_"), "_", sep="")
-    bait_nam <- get_1rdom_bait_per_gn(nn)
-
-    data4plot_same_cont <- get_data4plot_same_contig(gini_gene_rf, INFO_TARGENE_FILE, bait_nam)
-
-    P_Gn_obs <- data4plot_same_cont$P_Gn_obs
-    P_PMT_obs <- data4plot_same_cont$P_PMT_obs
-    P_Gn_PMT_obs <- data4plot_same_cont$P_Gn_PMT_obs
-    ds_Gn <- data4plot_same_cont$ds_Gn
-    ds_PMT <- data4plot_same_cont$ds_PMT
-    ds_Gn_PMT <- data4plot_same_cont$ds_Gn_PMT
-    distr_rdom_P_Gn_cont_Gn <- data4plot_same_cont$P_Gn_sim
-    distr_rdom_P_PMT_cont_PMT <- data4plot_same_cont$P_PMT_sim
-    distr_rdom_P_Gn_cont_PMT <- data4plot_same_cont$P_Gn_PMT_sim
+cat("\n")
+print("##### 1) observed sum of ranks")
+genes <- addZeroImpGenes(gini_contig_rf)
+group <- sapply(names(genes), get_elements)
+categ <- sort(unique(group))
+final_nber <- length(gini_contig_rf)
+sum_ranks <- get_rk_sum(names(genes), n_imp=final_nber, kept=length(genes), categ)
+sum_ranks <- data.frame(grp=categ, rnk=sum_ranks)
+rownames(sum_ranks) <- categ
 
 
-    cat("\n")
-    print("##### 3) P being same contig new random algo")
-    system.time(
-        sims2 <- mclapply(1:1000, function (x)
-            get_baits_per_pairs(x, bait_names=bait_nam, P_PMT=P_PMT_obs, P_Gn=P_Gn_obs, P_Gn_PMT=P_Gn_PMT_obs, info_TargGene_fil=INFO_TARGENE_FILE, gini_gene_rf=gini_gene_rf, ds_pmt=ds_PMT, ds_gns=ds_Gn, ds_gns_pmt=ds_Gn_PMT, inc=10, inc2=5, inc3=5, verbose=0)
-        , mc.cores=8)
-    )
+cat("\n")
+print("##### 2) P being same contig")
+nn <- paste(sapply(names(genes), collapse_elements, sep="_", what=1:2, colla="_"), "_", sep="")
+bait_nam <- get_1rdom_bait_per_gn(nn)
 
-    P_PMTs <- sapply(sims2, function(sol) sol$P_PMT)
-    P_Gns <- sapply(sims2, function(sol) sol$P_Gn)
-    P_Gns_PMTs <- sapply(sims2, function(sol) sol$PGn_PMT)
+data4plot_same_cont <- get_data4plot_same_contig(gini_gene_rf, INFO_TARGENE_FILE, bait_nam)
 
-    draw_P_same_contig(P_Gn_obs,
-        distr_rdom_P_Gn_cont_Gn, P_Gns, P_PMT_obs, distr_rdom_P_PMT_cont_PMT, P_PMTs, P_Gn_PMT_obs, distr_rdom_P_Gn_cont_PMT, P_Gns_PMTs)
+P_Gn_obs <- data4plot_same_cont$P_Gn_obs
+P_PMT_obs <- data4plot_same_cont$P_PMT_obs
+P_Gn_PMT_obs <- data4plot_same_cont$P_Gn_PMT_obs
+ds_Gn <- data4plot_same_cont$ds_Gn
+ds_PMT <- data4plot_same_cont$ds_PMT
+ds_Gn_PMT <- data4plot_same_cont$ds_Gn_PMT
+distr_rdom_P_Gn_cont_Gn <- data4plot_same_cont$P_Gn_sim
+distr_rdom_P_PMT_cont_PMT <- data4plot_same_cont$P_PMT_sim
+distr_rdom_P_Gn_cont_PMT <- data4plot_same_cont$P_Gn_PMT_sim
 
 
-    cat("\n")
-    print("##### 4) compute the expected distribution of ranks per gene category")
-            # 4.4.1) random drawing (Gns and PMTs distinguished)
+cat("\n")
+print("##### 3) P being same contig new random algo")
+system.time(
+    sims2 <- mclapply(1:1000, function (x)
+        get_baits_per_pairs(x, bait_names=bait_nam, P_PMT=P_PMT_obs, P_Gn=P_Gn_obs, P_Gn_PMT=P_Gn_PMT_obs, info_TargGene_fil=INFO_TARGENE_FILE, gini_gene_rf=gini_gene_rf, ds_pmt=ds_PMT, ds_gns=ds_Gn, ds_gns_pmt=ds_Gn_PMT, inc=10, inc2=5, inc3=5, verbose=0)
+    , mc.cores=8)
+)
+
+P_PMTs <- sapply(sims2, function(sol) sol$P_PMT)
+P_Gns <- sapply(sims2, function(sol) sol$P_Gn)
+P_Gns_PMTs <- sapply(sims2, function(sol) sol$PGn_PMT)
+
+draw_P_same_contig(P_Gn_obs,
+    distr_rdom_P_Gn_cont_Gn, P_Gns, P_PMT_obs, distr_rdom_P_PMT_cont_PMT, P_PMTs, P_Gn_PMT_obs, distr_rdom_P_Gn_cont_PMT, P_Gns_PMTs)
+
+
+cat("\n")
+print("##### 4) compute the expected distribution of ranks per gene category")
+        # 4.4.1) random drawing (Gns and PMTs distinguished)
 #~    N_bait_alpMat <- count_categ()
-    distr_rk_rdom_mat <- get_rdom_rk_mat(bait_nam, N_cate_rfGn, gini_gene_rf, INFO_TARGENE_FILE, gini_contig_rf)
+distr_rk_rdom_mat <- get_rdom_rk_mat(bait_nam, N_cate_rfGn, gini_gene_rf, INFO_TARGENE_FILE, gini_contig_rf)
 
-            # 4.4.2) random_LD drawing (Gns and PMTs distinguished)
-    distr_rk_rdom_LD_mat <- get_rdom_LD_rk_mat(sims2, bait_nam, gini_gene_rf, INFO_TARGENE_FILE, gini_contig_rf)
+        # 4.4.2) random_LD drawing (Gns and PMTs distinguished)
+distr_rk_rdom_LD_mat <- get_rdom_LD_rk_mat(sims2, bait_nam, gini_gene_rf, INFO_TARGENE_FILE, gini_contig_rf)
 
-            # 4.4.3) plots
-                # 4.4.3.1) length(gini_contig_rf) genes with importance and all genes used for the sum of ranks
-    distr_rdom_rk <- apply(distr_rk_rdom_mat, 2, get_rk_sum, n_imp=final_nber, kept=length(genes), categ)
-    distr_rdom_LD_rk <- apply(distr_rk_rdom_LD_mat, 2, get_rk_sum, n_imp=final_nber, kept=length(genes), categ)
-    draw_rk_distrib(sum_ranks, distr_rdom_rk, distr_rdom_LD_rk, final_nber, length(genes))
+        # 4.4.3) plots
+            # 4.4.3.1) length(gini_contig_rf) genes with importance and all genes used for the sum of ranks
+distr_rdom_rk <- apply(distr_rk_rdom_mat, 2, get_rk_sum, n_imp=final_nber, kept=length(genes), categ)
+distr_rdom_LD_rk <- apply(distr_rk_rdom_LD_mat, 2, get_rk_sum, n_imp=final_nber, kept=length(genes), categ)
+draw_rk_distrib(sum_ranks, distr_rdom_rk, distr_rdom_LD_rk, final_nber, length(genes))
 
-                # 4.4.3.1) for a subset of genes
-    dat_obs <- names(genes)
-    for (i in c(30, 50, final_nber))
-    {
-        print(paste("Sum of ranks over ", i, " baits (", i, " baits ranked from ", i, " (best) to 1 (less good))", sep=""))
-        # data obs
-        sum_ranks0 <- get_rk_sum(dat_obs, n_imp=i, kept=i, categ)
-        sum_ranks0 <- data.frame(grp=categ, rnk=sum_ranks0)
-        rownames(sum_ranks0) <- categ
+            # 4.4.3.1) for a subset of genes
+dat_obs <- names(genes)
+for (i in c(30, 50, final_nber))
+{
+    print(paste("Sum of ranks over ", i, " baits (", i, " baits ranked from ", i, " (best) to 1 (less good))", sep=""))
+    # data obs
+    sum_ranks0 <- get_rk_sum(dat_obs, n_imp=i, kept=i, categ)
+    sum_ranks0 <- data.frame(grp=categ, rnk=sum_ranks0)
+    rownames(sum_ranks0) <- categ
 
-        # simuls
-        distr_rdom_rk0 <- apply(distr_rk_rdom_mat, 2, get_rk_sum,
-            n_imp=i, kept=i, categ)
-        distr_rdom_LD_rk0 <- apply(distr_rk_rdom_LD_mat, 2, get_rk_sum,
-            n_imp=i, kept=i, categ)
-        # plots
-        res <- draw_rk_distrib(sum_ranks0, distr_rdom_rk0, distr_rdom_LD_rk0, i, i)
-        nfil <- sub(".txt", paste(i, ".txt", sep=""), SIGNIF)
-        write.table(res, file=nfil, row.names=F, quote=F, sep="\t")
-        print("Done")
-    }
-
-
-    cat("\n")
-    print("##### 5) draw the expected number of gene per categ in the top x")
-    for (i in c(30, 50, final_nber))
-    {
-        print(paste("Cunt baits par category over the top ", i, " baits", sep=""))
-        categ <- sum_ranks[,1]
-        obs_count <- get_count_top(names(gini_contig_rf), top=i, categ)
-        rdom_count <- apply(distr_rk_rdom_mat, 2, get_count_top,
-            top=i, categ)
-        rdom_LD_count <- apply(distr_rk_rdom_LD_mat, 2, get_count_top,
-            top=i, categ)
-        draw_count_distrib(top=i, categ, obs_count, rdom_count, rdom_LD_count)
-    }
+    # simuls
+    distr_rdom_rk0 <- apply(distr_rk_rdom_mat, 2, get_rk_sum,
+        n_imp=i, kept=i, categ)
+    distr_rdom_LD_rk0 <- apply(distr_rk_rdom_LD_mat, 2, get_rk_sum,
+        n_imp=i, kept=i, categ)
+    # plots
+    res <- draw_rk_distrib(sum_ranks0, distr_rdom_rk0, distr_rdom_LD_rk0, i, i)
+    nfil <- sub(".txt", paste(i, ".txt", sep=""), SIGNIF)
+    write.table(res, file=nfil, row.names=F, quote=F, sep="\t")
+    print("Done")
+}
 
 
-    cat("\n")
-    print("##### 6) save data")
-    argv <- commandArgs(TRUE)[1]
-	outFileName <- argv[1]
-    rm(argv)
-    ver(sprintf("Saving data to %s",outFileName))
-    save.image(file=outFileName)
-#~}
+cat("\n")
+print("##### 5) draw the expected number of gene per categ in the top x")
+for (i in c(30, 50, final_nber))
+{
+    print(paste("Cunt baits par category over the top ", i, " baits", sep=""))
+    categ <- sum_ranks[,1]
+    obs_count <- get_count_top(names(gini_contig_rf), top=i, categ)
+    rdom_count <- apply(distr_rk_rdom_mat, 2, get_count_top,
+        top=i, categ)
+    rdom_LD_count <- apply(distr_rk_rdom_LD_mat, 2, get_count_top,
+        top=i, categ)
+    draw_count_distrib(top=i, categ, obs_count, rdom_count, rdom_LD_count)
+}
 
 
+cat("\n")
+print("##### 6) save data")
+argv <- commandArgs(TRUE)[1]
+outFileName <- argv[1]
+rm(argv)
+ver(sprintf("Saving data to %s",outFileName))
+save.image(file=outFileName)
 
-#~if(DEBUG)
-#~	traceback(main(argv));
-#~if(!DEBUG)
-#~	main(argv);
 
 
 
