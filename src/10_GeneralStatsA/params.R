db7c3b23ec9fae42967833a899315e0e79868f67
lduvaux <l.duvaux@sheffield.ac.uk>
2014-04-22 21:35:24 +0100

    ibid

--- src/10_GeneralStatsA/params.R
+++
@@ -1,4 +0,0 @@
-#~ All explit parameters for the related script.R should be declared here as constantes ~#
-PREVIOUS_DATA <- "../03_ProcessingSegmentation.Rdata"
-RAW_DATA <- "../01_GetData.Rdata"
-
